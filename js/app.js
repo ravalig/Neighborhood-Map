8d55d6bb23a06e955a5f874387234a1ed23b15d5
Ravali <venubaburayini@Venubabus-MacBook-Pro.local>
2016-12-23 13:14:48 -0600

    initial map load

--- js/app.js
+++ js/app.js
@@ -1,8 +1,64 @@
-var map;
+//-----------------------------------------------------------------------------------------------------------------
+//    MODEL
+//-----------------------------------------------------------------------------------------------------------------
+
+placesList = [
+    {
+        name: 'Niagara Falls',
+        address: 'New Jersey',
+        center: {
+                    lat: 43.092461,
+                    lng: -79.047150
+                },
+        marker: {}
+    },
+    {
+        name: 'Honolulu',
+        address: 'Hawaii',
+        center: {
+                    lat: 21.315603,
+                    lng: -157.858093
+                },
+        marker: {}
+    },
+    {
+        name: 'Sanfrancisco',
+        address: 'California',
+        center: {
+                    lat:37.7749,
+                    lng: -122.4194
+                },
+        marker: {}
+    },
+    {
+        name: 'Yellow Stone National Park',
+        address: 'Wyoming',
+        center: {
+                    lat: 44.4280,
+                    lng: -110.5885
+                },
+        marker: {}
+    }
+]
+
+
       function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 2
-        });
+          var map;
+          map = new google.maps.Map(document.getElementById('map'), {
+          center: placesList[2].center,
+          zoom: 3
+          });
+
+          for(var i =0; i<placesList.length; i++){
+            placesList[i].marker = new google.maps.Marker({
+            position: placesList[i].center,
+            title: placesList[i].name,
+            map: map
+            });
+          }
       }
+
+//-----------------------------------------------------------------------------------------------------------------
+//      VIEWMODEL
+//-----------------------------------------------------------------------------------------------------------------
 
