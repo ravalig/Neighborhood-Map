bee20fde7b23dde9d62f61f90bbb110b84ae7040
Ravali <venubaburayini@Venubabus-MacBook-Pro.local>
2016-12-29 23:23:25 -0600

    Project working with all features

--- js/app.js
+++ js/app.js
@@ -2,19 +2,9 @@
 //    MODEL
 //-----------------------------------------------------------------------------------------------------------------
 var placesList =[];
-var markers = [];
 var map;
 var service;
 var infowindow;
-
-var Place = function(data){
-    this.name =  ko.observable(data.name);
-    this.place_id = ko.observable(data.place_id);
-    this.center = ko.observableArray([data.geometry.location.lat(),
-                                      data.geometry.location.lng()]);
-}
-
-this.error = ko.observable('');
 
 //-----------------------------------------------------------------------------------------------------------------
 //      VIEWMODEL
@@ -42,7 +32,6 @@ function createMap() {
         map.setZoom(13);
     });
 
-
     var request = {
                 location: NewYork,
                 radius: '1000',
@@ -63,14 +52,12 @@ function createMap() {
             $(".error").text("There is an error loading Google Places!!");
         }
     }
-
 }
 
 function googleError() {
-    this.error("There is an error loading Google Maps!!");
+    $(".error").text("There is an error loading Google Maps!!");
     return;
 }
-
 
 var ViewModel = function(){
 
@@ -79,97 +66,70 @@ var ViewModel = function(){
     this.showFilteredList = ko.observable(false);
     this.showYelp = ko.observable(false);
     this.query = ko.observable('');
-    this.placesArray = ko.observableArray([]);
+    this.locations = ko.observableArray([]);
     this.filteredPlaces = ko.observableArray([]);
-    this.yelpContent = ko.observable('');
-    this.map = map;
+    this.error = ko.observable('');
 
-    for(var i=0; i<10; i++){
-        self.placesArray.push(new Place(placesList[i]));
-    }
+    this.restaurant = ko.observable('');
+    this.phone = ko.observable('');
+    this.url = ko.observable('');
+    this.address = ko.observable('');
 
+    this.createLocations = function(locations){
+        for(var i=0; i<10; i++){
+            var location = {
+                        name: placesList[i].name,
+                        place_id : placesList[i].place_id,
+                        center:[ placesList[i].geometry.location.lat(),
+                                 placesList[i].geometry.location.lng()
+                               ],
+                        marker: new google.maps.Marker({
+                                    position: {
+                                                lat: placesList[i].geometry.location.lat(),
+                                                lng: placesList[i].geometry.location.lng()
+                                              },
+                                    title: placesList[i].name})
+                    };
+            locations.push(location);
 
-    this.createMarkers = function(places) {
-        /*
-            This function creates markers for the places passed as an argument
-        */
-        self.clearMarkers(markers);
-        markers = [];
-        for(var i=0; i<places.length; i++){
-            var mark = {value:  new google.maps.Marker({
-                                    position: {lat: places[i].center()[0],
-                                    lng: places[i].center()[1]},
-                                    title: places[i].name()}),
-                place_id:places[i].place_id()
-            };
-            markers.push(mark);
-        }
-    }
+            var marker = self.locations()[i].marker;
 
-    this.clearMarkers = function(markers){
-        /*
-            This function clears the markers on the map
-        */
-        for(var i=0; i<markers.length; i++){
-            markers[i].value.setMap(null);
-        }
-    }
-
-    this.displayMarkers = function(markers){
-    /*
-        This function displays markers on the map
-    */
-        for(var i=0; i<markers.length; i++){
-            markers[i].value.setMap(map);
-        }
-    }
-
-    this.onClickMarker = function(markers){
-    /*
-        When clicked on a marker, this function opens the infowindow
-        and animates the marker
-    */
-        for(var i=0; i<markers.length; i++){
-            markers[i].value.addListener('click', (function(marker){
+            marker.addListener('click', (function(marker) {
                 return function() {
+                    map.setZoom(18);
+                    map.setCenter(marker.getPosition());
+
                     if(infowindow){
                         infowindow.close(); //Closes any previously opened infoWindow
                     }
                     infowindow = new google.maps.InfoWindow({
-                        content: marker.value.title
+                        content: marker.title
                     });
 
-                    // if (marker.value.getAnimation() == null) {
-                    //     marker.value.setAnimation(google.maps.Animation.BOUNCE);
-                    // }
-                    // else {
-                    //     marker.value.setAnimation(null);
-                    // }
+                    marker.setAnimation(google.maps.Animation.BOUNCE);
+                    setTimeout(function() {
+                        marker.setAnimation(null);
+                    },2100);
 
-                    marker.value.setAnimation(google.maps.Animation.BOUNCE);
+                    infowindow.open(map, marker);
 
-                    infowindow.open(map, marker.value);
                 };
-            })(markers[i]));
+            })(marker));
         }
     }
 
-    this.openInfoWindow = function(map, markers){
-        infowindow = new google.maps.InfoWindow({
-                        content: markers[0].value.title
-                    });
-        infowindow.open(map, markers[0].value);
-        if (markers[0].value.getAnimation() == null) {
-            markers[0].value.setAnimation(google.maps.Animation.BOUNCE);
-        }
-        else {
-            markers[0].value.setAnimation(null);
+    this.displayMarkers = function(locations){
+        /*
+            This function displays markers on the map
+        */
+        for(var i=0; i<locations.length; i++){
+            locations[i].marker.setMap(map);
         }
     }
 
-    this.createMarkers(this.placesArray());     // Initially, create markers for all locations
-    this.displayMarkers(markers);               // Displays markers on the map
-    this.onClickMarker(markers);            // Displays infoWindow when clicked on markers
+    this.createLocations(this.locations());
+    this.displayMarkers(this.locations());
+
 
     this.displayYelpDetails = function(clickedItem){
     /*
@@ -181,7 +141,9 @@ var ViewModel = function(){
             }
 
         var yelpUrl = 'https://api.yelp.com/v2/search/?term='+
-                        clickedItem.name();
+                        clickedItem.name;
+
+        console.log(clickedItem.name);
 
         var YELP_KEY = 'jdNHl27QxX5TBIk5SeBOCg',
             YELP_TOKEN = 'ujhYkF8kbD1xqi0zleUgNMfWdGUp_qyx',
@@ -211,65 +173,43 @@ var ViewModel = function(){
         dataType: 'jsonp',          //from adding on a cache-buster parameter "_=23489489749837",
         jsonpCallback: 'cb',        //invalidating our oauth-signature
         success: function(results) {
-                    self.showYelp(true);
-                    $('.yelp').text("");
-                    console.log(results.businesses[0].review_count);
-                    var temp;
-                    temp =  '<h4>'+results.businesses[0].name+'</h4>'+
-                            '<div>'+
-                                '<img '+'src='+results.businesses[0].rating_img_url+'>'+'<br>'+
-                                'Phone:  ' + results.businesses[0].display_phone+'<br>'+
-                                 results.businesses[0].location.display_address+'<br>'+
-                                '</div>'+
-                            '<img class="yelpImg" src="img/yelp.png">';
-                    $('.yelp').append(temp);
+                    console.log(results.businesses[0].name);
+                    self.restaurant(results.businesses[0].name);
+                    self.url(results.businesses[0].rating_img_url);
+                    self.phone(results.businesses[0].display_phone);
+                    self.address(results.businesses[0].display_address);
                 },
         error: function(error) {
-                    this.error("There is an error loading yelp data");
+                    self.error("There is an error loading yelp data");
                 }
         };
         $.ajax(settings);
     }
 
-
     this.listClick = function(clickedItem){
-    /*
-        When clicked on a list item, this function does the following:
-            - Clears previous markers on the map
-            - Creates a marker for the list item
-            - Displays the marker on the map
-            - Opens the info window and animates the marker
-            - Displays the yelp details asociated for that location
-    */
-        self.clearMarkers(markers);
-        self.createMarkers([clickedItem]);
-        self.displayMarkers(markers);
-        self.onClickMarker(markers);
-        self.openInfoWindow(map, markers);
+        /*
+            When clicked on a list item, this function does the following:
+                - Opens the info window and animates the marker
+                - Displays the yelp details asociated for that location
+        */
+        google.maps.event.trigger(clickedItem.marker, 'click');
+        self.showYelp(true);
         self.displayYelpDetails(clickedItem);
     }
 
     this.filterQuery = function(){
-    /*
-        When user inputs value to filter, this function does the following:
-            - Displays the location items that contains the entered input
-            - clears previous markers on the map
-            - creates markers  for the filtered items
-            - displays markers on the map
-            - Opens the info window
-            - When clicked on the marker animates the marker
-    */
+
         self.filteredPlaces.removeAll();
-        for( var i=0; i<self.placesArray().length; i++){
-            if(self.placesArray()[i].name().toLowerCase().indexOf(self.query()) > -1){
-                self.filteredPlaces.push(self.placesArray()[i]);
+        for( var i=0; i<self.locations().length; i++){
+            if(self.locations()[i].name.toLowerCase().indexOf(self.query()) > -1){
+                self.locations()[i].marker.setVisible(true);
+                self.filteredPlaces.push(self.locations()[i]);
+            }
+            else{
+                self.locations()[i].marker.setVisible(false);
             }
         }
         self.showList(false);
         self.showFilteredList(true);
-        self.clearMarkers(markers);
-        self.createMarkers(self.filteredPlaces());
-        self.displayMarkers(markers);
-        self.onClickMarker(markers);
     }
 }
